091649d0a4690c2d623e27307de4c72104f91c8d
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2012-08-03 13:36:57 +0200

    Added a setup.py

--- README.rst
+++ README.rst
@@ -17,6 +17,25 @@ The list of all paths is available from the ``paths`` command::
 
   $ hg paths
 
+
+Alternatively you can install hgallpaths with ``pip``::
+
+  $ pip install hgallpaths
+
+or with ``setuptools``::
+
+  $ python setup.py install
+
+With either of those methods you don't need to specify the path to hgallpaths
+in your hgrc configuration::
+
+  [extensions]
+  hgallpaths = /path/to/hgallpaths.py
+
+
+Configuration
+-------------
+
 You can exclude certain paths in the configuration. For example to always
 exclude the path ``foo`` and ``bar``, as well as exclude ``baz`` when pushing
 you add this to your ~/.hgrc (or repo hgrc)::
---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='hgallpaths',
+    version='0.1',
+    author='Keegan Carruthers-Smith',
+    author_email='keegan.csmith@gmail.com',
+    url='https://bitbucket.org/keegan_csmith/hgallpaths',
+    license='LICENSE.txt',
+    py_modules=['hgallpaths'],
+    description=('A mercurial extension to allow pushing and pulling from '
+                 'all paths.'),
+    long_description=file('README.rst').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+    ],
+)
